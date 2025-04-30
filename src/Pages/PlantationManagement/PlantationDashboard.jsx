dc59768988b135fc621b905110c1eef52ffbb154
DamikaDeshan <damikadeshan40@gmail.com>
2025-04-30 11:45:33 +0530

    Update PlantationDashboard.jsx

--- src/Pages/PlantationManagement/PlantationDashboard.jsx
+++ src/Pages/PlantationManagement/PlantationDashboard.jsx
@@ -12,6 +12,8 @@ import {
   WifiIcon,
   CheckCircleIcon,
   MapPinIcon,
+  DocumentTextIcon,  // Add this
+  BugAntIcon        // Add this
 } from "@heroicons/react/24/outline";
 import PlantationSidebar from "../../Components/PlantationSidebar";
 
